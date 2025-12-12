e2d6f6971a3387cb9b0ab75089e67c594ffc7734
GustavoFS <gustavoferreirasantosg@gmail.com>
2025-12-12 18:50:45 -0300

    mudanças para realizar o deploy

--- src/main/java/br/com/ecommerce/model/Product.java
+++ src/main/java/br/com/ecommerce/model/Product.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Entity(name = "product")
+@Table(name = "product", schema = "ecommerce")
 public class Product {
 
     @Id @GeneratedValue(strategy = GenerationType.UUID)
